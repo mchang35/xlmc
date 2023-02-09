698c4512b4c1bb4810d257a9133730190f0537d0
mchang35 <mandy_chang@brown.edu>
2023-02-08 22:59:14 -0600

    some more functionality

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
+function loginQuestion() {
+    // on load for the div
+}
+
 function scrollToLogin() {
 
 }
 
 function login() {
-    
+    // if answer is correct
+
+    // if answer is not correct
 }
